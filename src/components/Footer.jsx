60ce6cc97041be1df0738bb42550c5be0a13a501
Billie Zhang <billie.zhang1504@gmail.com>
2023-03-18 15:25:51 -0400

    Updated contact in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,38 @@
 import styles from '../style';
 import { logo } from "../assets";
+import { footerLinks, socialMedia } from "../constants";
 
 const Footer = () => (
 	<section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
       <div className='flex-[1] flex flex-col justify-start mr-10'>
         <img src={logo} alt="EcoThreads" className='w-[266px] object-contain' />
-        <p className={`${styles.paragraph} mt-4 max-w-[312px]`}>
-          A new way to make fashion more sustainable than ever
+        <p className={`${styles.paragraph} mt-0 max-w-[312px]`}>
+          Sustainable fashion made easy
         </p>
+      </div>
+
+      <div className="flex-[1.5] w-full flex flex-row  flex-wrap md:mt-0 mt-10">
+        {footerLinks.map((footerlink) => (
+          <div key={footerlink.title} className={`flex flex-col ss:my-0 my-4 min-w-[150px]`}>
+            <h4 className="font-poppins font-medium text-[18px] leading-[27px] text-white">
+              {footerlink.title}
+            </h4>
+            <ul className="list-none mt-4">
+              {footerlink.links.map((link, index) => (
+                <li
+                  key={link.name}
+                  className={`font-poppins font-normal text-[16px] leading-[24px] text-dimWhite hover:text-secondary 
+                  ${
+                    index !== footerlink.links.length - 1 ? "mb-4" : "mb-0"
+                  }`}
+                >
+                  {link.name}
+                </li>
+              ))}
+            </ul>
+          </div>
+        ))}
       </div>
     </div>
 
@@ -16,8 +40,20 @@ const Footer = () => (
       <p className="font-poppins font-normal text-center text-[18px] leading-[27px] text-white">
         Copyright Ⓒ 2023 EcoThreads. All Rights Reserved.
       </p>
+      <div className="flex flex-row md:mt-0 mt-6">
+        {socialMedia.map((social, index) => (
+          <img
+            key={social.id}
+            src={social.icon}
+            alt={social.id}
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${
+              index !== socialMedia.length - 1 ? "mr-6" : "mr-0"
+            }`}
+            onClick={() => window.open(social.link)}
+          />
+        ))}
+      </div>
     </div>
-
   </section>
 );
 
