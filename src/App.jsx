463269e8a70b441cc9c55afca1d8f874d0fefbbe
victoriachakma <victoriac.6535@gmail.com>
2023-03-18 14:04:59 -0400

    NavBar completed
    Includes new logo image + button

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,17 @@ const App = () => (
     
   <div className="bg-olive w-full overflow-hidden">
   
-     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+    <div className={`${styles.boxWidth}`}>
+      <Navbar />
+    </div>
+    </div>
+
+    <div className={` bg-olive ${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
-        <Navbar />
+        <Footer />
       </div>
-     </div>
-
-      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-        <div className={`${styles.boxWidth}`}>
-          <Navbar />
-        </div>
-      </div>
-
-        <div className={` bg-olive ${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Footer />
-          </div>
-        </div>
+    </div>
 
   </div>
 
