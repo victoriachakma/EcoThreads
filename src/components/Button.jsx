6db14f4f9d0c44a48be328215c360c5401ffab85
victoriachakma <victoriac.6535@gmail.com>
2023-03-19 11:24:52 -0400

    Updates

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ styles }) => (
-	<button type="button" className={`py-4 px-6 font-poppins font-medium text-[18px] text-olive bg-tea rounded-[10px] outline-none ${styles}`}>
-    Download
-  </button>
-)
-
-export default Button;
