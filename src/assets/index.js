235b2b83782c7062dfbe60a41d4a2320f96e73bc
victoriachakma <victoriac.6535@gmail.com>
2023-03-18 21:18:51 -0400

    First draft of About/Team section

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,10 +1,13 @@
-
 import logo from "./logo.png";
 import facebook from "./facebook.svg";
 import instagram from "./instagram.svg";
 import linkedin from "./linkedin.svg";
 import twitter from "./twitter.svg";
 import longlogo from "./longlogo.png";
+import bollo from "./bollo.jpg";
+import grace from "./grace.jpg";
+import kelly from "./kelly.jpg";
+import vic from "./vic.jpg";
 
 
 export {
@@ -14,4 +17,8 @@ export {
   linkedin,
   twitter,
   longlogo,
+  bollo,
+  grace,
+  kelly,
+  vic,
 };
