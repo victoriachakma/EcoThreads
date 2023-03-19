d9d2fa345412db1cbb592ec795193f7bab025367
victoriachakma <victoriac.6535@gmail.com>
2023-03-18 23:10:35 -0400

    Hero update...

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,14 @@
 import styles from '../style';
 
 const Hero = () => (
-	<section id='home' className={`flex md: flex-row flex-col ${styles.paddingY}`}>
-    <div className='flex flex-row justify-between items-center w-full'>
-    <h1 className='flex-1 font-poppins font-semibold text-center s:text-[72px] text-[52px] text-olive ss:leading-[100px] leading-[75px]'>
-      Your closet. 
-    </h1>
+	<section id='home' className={`flex md: flex-col ${styles.paddingY}`}>
+    <div className='justify-between items-center w-full'>
+      <h1 className='flex-1 font-poppins font-semibold text-olive text-center text-[72px]'>
+        Sustainable fashion made easy.
+      </h1>
+      <h2 className='flex-1 font-poppins font-light text-olive text-center text-[36px] px-60 pt-5'>
+        EcoThreads is the go to app for outfit inspiration while reducing your global carbon footprint. 
+      </h2>
     </div>
   </section>
 )
